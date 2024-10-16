dc353f2a0c4723ee261bf03981e39dbfcbb104f7
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-10-16 22:54:14 +0300

    added get function for queue

--- queue.h
+++ queue.h
@@ -4,32 +4,34 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct Queue {
+typedef struct queue_t {
     void** items;
 
     int capacity;
     int stored;
-} Queue;
+} queue_t;
 
-void create_queue(Queue* queue, int initial_capacity);
+void create_queue(queue_t* queue, int initial_capacity);
 
-void queue_push(Queue* queue, void* item);
+void queue_push(queue_t* queue, void* item);
 
-void* queue_pop(Queue* queue);
+void* queue_get(queue_t* queue, int index);
 
-int queue_empty(Queue* queue);
+void* queue_pop(queue_t* queue);
 
-int queue_size(Queue* queue);
+int queue_empty(queue_t* queue);
 
-int queue_capacity(Queue* queue);
+int queue_size(queue_t* queue);
 
-void free_queue(Queue* queue);
+int queue_capacity(queue_t* queue);
 
-void free_queue_content(Queue* queue);
+void free_queue(queue_t* queue);
+
+void free_queue_content(queue_t* queue);
 
 #ifdef HAUL_IMPLEMENTATION
 
-void create_queue(Queue* queue, int initial_capacity) {
+void create_queue(queue_t* queue, int initial_capacity) {
     assert(initial_capacity > 0);
 
     queue->items = calloc(initial_capacity, sizeof(void*));
@@ -38,7 +40,7 @@ void create_queue(Queue* queue, int initial_capacity) {
     queue->capacity = initial_capacity;
 }
 
-void queue_push(Queue* queue, void* item) {
+void queue_push(queue_t* queue, void* item) {
     if(queue->stored + 1 > queue->capacity) {
         queue->capacity = queue->capacity * 2;
 
@@ -50,7 +52,15 @@ void queue_push(Queue* queue, void* item) {
     ++queue->stored;
 }
 
-void* queue_pop(Queue* queue) {
+void* queue_get(queue_t* queue, int index) {
+    if(queue->stored <= index) {
+        return NULL;
+    }
+
+    return queue->items[index];
+}
+
+void* queue_pop(queue_t* queue) {
     if(queue->stored == 0) {
         return NULL;
     }
@@ -68,23 +78,23 @@ void* queue_pop(Queue* queue) {
     return popped_value;
 }
 
-int queue_empty(Queue* queue) {
+int queue_empty(queue_t* queue) {
     return queue->stored == 0;
 }
 
-int queue_size(Queue* queue) {
+int queue_size(queue_t* queue) {
     return queue->stored;
 }
 
-int queue_capacity(Queue* queue) {
+int queue_capacity(queue_t* queue) {
     return queue->capacity;
 }
 
-void free_queue(Queue* queue) {
+void free_queue(queue_t* queue) {
     free(queue->items);
 }
 
-void free_queue_content(Queue* queue) {
+void free_queue_content(queue_t* queue) {
     for(int i = 0; i < queue->stored; ++i) {
         free(queue->items[i]);
     }
